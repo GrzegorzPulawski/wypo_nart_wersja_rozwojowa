00d5afca0c5bc52462dc74885b502e26e7d100b8
Grzegorz Pulawski <szkolanakrawedzi@gmail.com>
2022-08-03 23:06:20 +0200

    nadal blad 404

--- src/main/java/controller/NartyController.java
+++ src/main/java/controller/NartyController.java
@@ -1,15 +1,17 @@
 package controller;
 
+import dto.NartyDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import model.Narty;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import repository.NartyRepository;
 import service.NartyService;
 
+import java.util.List;
+
 @Slf4j
-@RequestMapping("/api/narty")
+@RequestMapping("/api")
 @RestController
 @RequiredArgsConstructor
 public class NartyController {
@@ -20,6 +22,12 @@ public class NartyController {
     public void createNarty(@RequestBody Narty narty){
         log.info("Request create ->" + narty);
         nartyService.addNarty(narty);
+    }
+    @GetMapping
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<NartyDTO> nartyDTOList() {
+        log.info("Request: list");
+        return nartyService.findAll();
     }
 
 }
--- src/main/java/service/NartyService.java
+++ src/main/java/service/NartyService.java
@@ -1,10 +1,16 @@
 package service;
 
+import dto.NartyDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import model.Narty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 import repository.NartyRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -14,6 +20,15 @@ public class NartyService {
 
     public void addNarty(Narty narty) {
         nartyRepository.save(narty);
+    }
 
+    public List<NartyDTO> findAll() {
+        List<Narty> nartyList = nartyRepository.findAll();
+
+        List<NartyDTO> nartyzListy = new ArrayList<>();
+        for (Narty narty : nartyList) {
+            nartyzListy.add(narty.mapNartDTO());
+        }
+        return nartyzListy;
     }
 }
