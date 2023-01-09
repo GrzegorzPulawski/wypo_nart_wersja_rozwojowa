a62af9d3694632f27689156108407218f8d28bf1
Grzegorz Pulawski <szkolanakrawedzi@gmail.com>
2023-01-09 12:42:46 +0100

    wersja rozwojowa

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/',
+        '/frontend',
         createProxyMiddleware({
             target: 'http://localhost:8090',
             changeOrigin: true,
